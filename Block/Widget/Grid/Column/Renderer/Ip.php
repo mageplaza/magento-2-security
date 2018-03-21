96e832740715d50c672a41bbbb7a4dec41b2afa5
btn114 <nghiabt@mageplaza.com>
2018-03-21 08:51:42 +0700

    ad ip column in admin user grid

---
+++ Block/Widget/Grid/Column/Renderer/Ip.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_Security
+ * @copyright   Copyright (c) 2018 Mageplaza (https://www.mageplaza.com/)
+ * @license     http://mageplaza.com/LICENSE.txt
+ */
+
+namespace Mageplaza\Security\Block\Widget\Grid\Column\Renderer;
+
+use Magento\Backend\Block\Context;
+use Mageplaza\Security\Model\LoginLogFactory;
+/**
+ * Class Status
+ * @package Mageplaza\Security\Block\Widget\Grid\Column\Renderer
+ */
+class Ip extends \Magento\Backend\Block\Widget\Grid\Column\Renderer\AbstractRenderer
+{
+    /**
+     * @var LoginLogFactory
+     */
+    protected $_logFactory;
+
+    /**
+     * LastTimeLogin constructor.
+     * @param \Mageplaza\Security\Model\LoginLogFactory $logFactory
+     * @param Context $context
+     * @param array $data
+     */
+    public function __construct
+    (
+        LoginLogFactory $logFactory,
+        Context $context,
+        array $data = []
+    )
+    {
+        $this->_logFactory = $logFactory;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Renders grid column
+     *
+     * @param   \Magento\Framework\DataObject $row
+     * @return  string
+     */
+    public function render(\Magento\Framework\DataObject $row)
+    {
+        $userName = $row->getData('username');
+        $lastLog = $this->_logFactory->create()->getCollection()
+            ->addFieldToFilter('user_name', $userName)
+            ->addFieldToFilter('status', 1)
+            ->getLastItem();
+
+        return '<a href="http://www.traceip.net/?query='.$lastLog->getIp().'" target="_blank">'.$lastLog->getIp().'</a>';
+    }
+}
